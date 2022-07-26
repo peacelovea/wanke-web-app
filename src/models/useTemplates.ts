6d4c4f597928d140c71096f67ec1977b4f672ff7
nadila <nadila@zhihu.com>
2022-07-26 15:56:15 +0800

    feat: 模板条数

--- src/models/useTemplates.ts
+++ src/models/useTemplates.ts
@@ -3,7 +3,9 @@ import { fetchTemplateList } from '@/services';
 import { createModel } from 'hox';
 
 const useTemplates = () => {
-  const { data } = useRequest<{ data: { list: any[] } }>(() => fetchTemplateList());
+  const { data } = useRequest<{ data: { list: any[] } }>(() =>
+    fetchTemplateList({ page_size: 200 }),
+  );
 
   return {
     templates: data?.list || [],
